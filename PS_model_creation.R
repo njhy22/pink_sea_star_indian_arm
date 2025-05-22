f4337987203e22bea1978218c7c022a862d07f5b
Nicole Yang <nicole.jhyang22@gmail.com>
2025-05-22 14:00:13 -0700

    r script for models and transect abundance data loaded

---
+++ PS_model_creation.R
@@ -0,0 +1,29 @@
+#Title: Model 1 Pink Sea Star
+#Author: Nicole Yang
+#Date: May 22, 2025
+
+library(usethis)
+use(git)
+
+# Load packages
+library(tidyverse)
+library(MetBrewer)
+
+# Load CSV files 
+
+
+
+##### MODEL 1: HISTORICAL VS PRESENT #######
+
+
+
+
+
+
+##### MODEL 2: ROV VS TRANSECT ##########
+
+
+
+
+
+##### MODEL 3: LIFE HISTORY: PRESENT #######
