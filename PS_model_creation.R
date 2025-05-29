48fe25208724cd12eda3a5c2aedb907c4bb67528
Nicole Yang <nicole.jhyang22@gmail.com>
2025-05-29 16:31:59 -0700

    adding measurements from Feb 5 ROV. Isolate 1 site with just location and diameter. can caluculate mean for 1 site, unsure how to combine them all into 1 dataset to graph for all sites together

--- PS_model_creation.R
+++ PS_model_creation.R
@@ -49,17 +49,67 @@ rov_measurement$dive_id <- gsub("^(MI_M19_R1|MI_A5_R2)$", "Moody Inlet", rov_mea
 rov_measurement$dive_id <- gsub("^(GR_M19_R1|GR_A9_R2)$", "Grey Rocks Island", rov_measurement$dive_id, ignore.case = TRUE)
 rov_measurement$dive_id <- gsub("^(CP_M31_R1|CP_A9_R2)$", "Cate's Park", rov_measurement$dive_id, ignore.case = TRUE)
 rov_measurement$dive_id <- gsub("^(BC_M26_R1|BC_M31_R2)$", "Belcarra Bay", rov_measurement$dive_id, ignore.case = TRUE)
+rov_measurement$diameter <- gsub("^(N/A)$", "0", rov_measurement$diameter, ignore.case = TRUE) #turned N/a into 0 values so it can be numeric 
 
 #double check site names are the same for both csv files 
-unique(rov_measurement$dive_id)
-unique(historic_measurement$Location)
+unique(rov_measurement$location)
+unique(historic_measurement$location)
 
 #try to plot, not what we want though 
-ggplot(data_2, aes(x = depth, y = diameter)) +
+## sample plot for present sites and diameter, raw data 
+ggplot(rov_measurement, aes(x = location, y = diameter)) +
   geom_point() +
-  ggtitle("Present Diameter Size") +
-  xlab("Depth") +
+  ggtitle("location and diameter") +
+  xlab("location") +
   ylab("Diameter")
+
+
+
+##sample plot for historic sites and diameter, raw data
+ggplot(historic_measurement, aes(x = location, y = diameter)) +
+  geom_point() +
+  ggtitle("location and diameter") +
+  xlab("location") +
+  ylab("Diameter")
+
+#classification of each variable 
+class(rov_measurement$diameter)
+rov_measurement$diameter <- as.numeric(as.character(rov_measurement$diameter))
+
+#overall averages for each sheet 
+mean(rov_measurement$diameter, na.rm = TRUE) #average for overall diameter for present
+mean(historic_measurement$diameter, na.rm = TRUE) #average for overall diameter for historic
+
+#for each site average?? 
+data_2<- rov_measurement %>% 
+  filter(location == "Cate's Park")
+mean(data_2$diameter)
+
+#isolate by 1 site, keep location and diameter (2 columns) in this dataframe
+location_df <- rov_measurement %>%
+  filter(location == "Bedwell Bay") %>%
+  group_by(location, diameter) %>%
+  summarise(total = n()) 
+
+mean(location_df$diameter, na.rm = TRUE)
+
+#plot for 1 site?? with diameter frequencies?? 
+ggplot(location_df, aes(x = diameter, y = total)) +
+  geom_point() +
+  ggtitle("location and diameter") +
+  xlab("diameter") +
+  ylab("frequency")
+
+
+#merging the 2 csv files 
+rov_present <- rov_measurement[, c("location", "diameter")]
+rov_historic <- historic_measurement[, c("location", "diameter")]
+
+
+
+combined_rov <- cbind(rov_present, rov_historic)
+
+##### MODEL 1: HISTORICAL VS PRESENT #######
 
 #isolate data set for 1 site
 data_2<- rov_measurement %>% 
@@ -68,14 +118,6 @@ data_2<- rov_measurement %>%
 #isolate historic data set for 1 site
 data_CP<- historic_measurement %>% 
   filter(Location == "Cate's Park")
-
-#attempt to isolate just diameter column so i can plot with the other csv file, but i don't think its working
-diameter_present_df <- rov_measurement[["diameter"]]
-
-
-
-
-##### MODEL 1: HISTORICAL VS PRESENT #######
 
 
 
